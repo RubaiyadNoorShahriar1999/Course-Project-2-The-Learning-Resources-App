695628f36a4a3e85a280eddec78a663e3a7f6aef
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-05-30 18:14:13 +0600

    Tab switching using provide and inject done for the resources

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,35 +1,32 @@
 <template>
-  <ul>
-    <li>
-      <learning-resource v-for="res in storedResources" :key="res.id">
-      </learning-resource>
-    </li>
-  </ul>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
 export default {
   components: {
-    LearningResource,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'official-guide',
-          description: 'The official documentation to Vue JS',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://vuejs.org',
-        },
-      ],
-    };
+    TheHeader,
+    TheResources,
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
