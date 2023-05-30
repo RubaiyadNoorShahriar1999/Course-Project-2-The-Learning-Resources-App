dd36036905b1c5e202a3ac39a92d045db76bb89a
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-05-30 16:17:53 +0600

    Starter Code uploaded for Project 2

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li>
+      <learning-resource v-for="res in storedResources" :key="res.id">
+      </learning-resource>
+    </li>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'official-guide',
+          description: 'The official documentation to Vue JS',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+  </li>
+</template>
+<script></script>
